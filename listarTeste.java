2ec5d227ffc16dfd3ae4928a130a9194da6949b8
Lan1526 <153443607+Lan1526@users.noreply.github.com>
2024-07-18 23:25:31 -0300

    Update listarTeste.java

--- listarTeste.java
+++ listarTeste.java
@@ -10,11 +10,11 @@ public class listarTeste extends BaseTest {
     @Test
     public void validarlista(){
         ListarDespesas page=new ListarDespesas(getDriver());
-        String resultadoFinal=page.informarBaseFísica1("POA").informarMes1("JANEIRO").informarCredor1("CEEE").informarDespesa1("ENERGIA ").informarValor1("180").informarVencimento1("12/12/2024").resultadoLista();
+        String resultadoFinal=page.informarBaseFísica1("POA").informarMes1("JANEIRO").informarCredor1("CEEE").informarDespesa1("ENERGIA ELÉTRICA ").informarValor1("180").informarVencimento1("12/12/2024").resultadoLista();
 
         Assert.assertTrue(resultadoFinal.contains("POA"));
         Assert.assertTrue(resultadoFinal.contains("CEEE"));
-        Assert.assertTrue(resultadoFinal.contains("ENERGIA "));
+        Assert.assertTrue(resultadoFinal.contains("ENERGIA ELÉTRICA "));
         Assert.assertTrue(resultadoFinal.contains("180"));
         Assert.assertTrue(resultadoFinal.contains("12/12/2024"));
         Assert.assertTrue(resultadoFinal.contains("JANEIRO"));
